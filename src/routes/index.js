b58eec31f7277e1a47a0f35f155f07a6ecd80373
graciela nieto <graciela.nieto.ayala@gmail.com>
2022-11-25 02:05:14 -0500

    fixes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const adminRoute = require('./adminRoute.js')
 const userRoute = require('./userRoute.js')
 const productRoute = require('./productRoute.js')
-const purchaseRoute = require('./purchaseRoute')
+//const purchaseRoute = require('./purchaseRoute')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -11,7 +11,7 @@ const router = Router();
 router.use('/admin', adminRoute)
 router.use('/user', userRoute)
 router.use('/products', productRoute)
-router.use('/purchase', purchaseRoute )
+//router.use('/purchase', purchaseRoute)
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
