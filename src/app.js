016ecc1218e9b0730e651aa8dfc7a5b7d2afe0bb
Felipe Akvaar Medina Carrillo <medina.766@hotmail.com>
2022-11-26 16:02:20 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header(
       'Access-Control-Allow-Origin',
-      'https://h-couture-odxfhjkfia-uc.a.run.app/'
+      '*'
   ) // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
