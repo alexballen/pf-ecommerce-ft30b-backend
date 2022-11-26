20312227fecb553f0b0f6219668ab1e6875ce3af
Felipe Akvaar Medina Carrillo <medina.766@hotmail.com>
2022-11-26 16:12:30 -0600

    Update adminRoute.js

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -4,6 +4,6 @@ const {toggleProductAsFeatured} = require('../controllers/adminController')
 
 const adminRoute = Router()
 
-adminRoute.post('/featured/.productId', toggleProductAsFeatured)
+adminRoute.post('/featured/:productId', toggleProductAsFeatured)
 
 module.exports = adminRoute
